e5b1ef10146b7fd6932ca9670c218e52ce6cd428
Achmad Ibrahim Humam <55597186+humamisme@users.noreply.github.com>
2022-09-19 22:00:23 +0700

    fix promiseOutput function

--- promise.js
+++ promise.js
@@ -1,7 +1,27 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
+  let hitungEmosi = 0;
+
+  await new Promise(resolve => {
+    for (object of theaterIXX) {
+      if (object.hasil === emosi) hitungEmosi++;
+    }
+    return resolve(hitungEmosi);
+  });
+
+  await new Promise(resolve => {
+    for (object of theaterVGC) {
+      if (object.hasil === emosi) hitungEmosi++;
+    }
+    return resolve(hitungEmosi);
+  });
+
+  return hitungEmosi;
+}
 
 module.exports = {
   promiseOutput,
